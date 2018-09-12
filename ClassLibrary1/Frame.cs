023ec3d39c134093a682150aacb4b8c977187643
Alexander James Bochel <alexander.bochel@excella.com>
2018-09-12 10:17:13 -0400

    Finalized functionality for using the Frame class in the Bowling Game

--- ClassLibrary1/Frame.cs
+++ ClassLibrary1/Frame.cs
@@ -9,11 +9,16 @@ namespace BowlingGame
 		private int _totalScoreForFrame;
 		private int _scoreForFirstRoll;
 		private int _scoreForSecondRoll;
+		private int _rollNumber;
+
+		public Frame()
+		{
+			// Empty 
+		}
 
 		public int totalScoreForFrame
 		{
-			get { return _totalScoreForFrame; }
-			set { _totalScoreForFrame = value; }
+			get { return _scoreForSecondRoll + _scoreForFirstRoll; }
 		}
 
 		public int scoreForFirstRoll
@@ -26,6 +31,12 @@ namespace BowlingGame
 		{
 			get { return _scoreForSecondRoll; }
 			set { _scoreForSecondRoll = value; }
+		}
+
+		public int rollNumber
+		{
+			get { return _rollNumber; }
+			set { _rollNumber = value; }
 		}
 	}
 }
--- ClassLibrary1/Game.cs
+++ ClassLibrary1/Game.cs
@@ -8,28 +8,40 @@ namespace BowlingGame
 	{
 		private int _score = 0;
 		private List<Frame> frames = new List<Frame>();
-		private int currentRoll = 0;
+		private int currentFrame = 0;
 
 		public Game()
 		{
-			
+			for (int i = 0; i < 10; i++)
+			{
+				frames.Add(new Frame());
+			}
 		}
 
 		public void roll(int pointsScored)
 		{
-			
+			Frame frameForCurrentRoll = frames[currentFrame];
 
-			_score += pointsScored;
+			if (frameForCurrentRoll.rollNumber == 0)
+			{
+				frameForCurrentRoll.scoreForFirstRoll = pointsScored;
+				frameForCurrentRoll.rollNumber++;
+			}
+			else
+			{
+				frameForCurrentRoll.scoreForSecondRoll = pointsScored;
+				currentFrame++;
+			}
 		}
 
 		public int score()
 		{
-			foreach (Frame allFrames in frames)
+			foreach (Frame currentFrame in frames)
 			{
-				
+				_score += currentFrame.totalScoreForFrame;
 			}
 
-			return -1;
+			return _score;
 		}
 	}
 }
